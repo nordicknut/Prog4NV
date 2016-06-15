51cbd4de6d0751bb90f060c12b39366cb68de99b
jpbra <jpbrastad@gmail.com>
2016-06-15 00:00:07 -0700

    I hit the save button.

--- Prog4NV/Prog4NV.cpp
+++ Prog4NV/Prog4NV.cpp
@@ -1,11 +1,54 @@
-// Prog4NV.cpp : Defines the entry point for the console application.
+
+// Prog4.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
-
-
-int main()
+#include "BST.h"
+using namespace std;
+// CS212, Spring 2014, Program #5
+void main()
 {
-    return 0;
-}
+	// Create a binary search tree for strings
+	BST<string> sTree;
 
+	// Add elements to the tree
+	sTree.insert("America");
+	sTree.insert("Canada");
+	sTree.insert("Russia");
+	sTree.insert("France");
+	sTree.insert("Mexico");
+
+	// Create an Iterator
+	Iterator<string> iterator = sTree.begin();
+
+	sTree.insert("Texas"); // modify the tree
+
+						   // Traverse a binary tree using iterators
+	while (iterator != sTree.end())
+	{
+		cout << *iterator << endl;
+		iterator++;
+	}
+	// Texas is missing, FIX IT!!!!
+
+	for (string& s : sTree)
+	{
+		cout << s << endl;
+	}
+
+	sTree.clear();
+
+	BST<int> iTree;
+
+	iTree.insert(5);
+	iTree.insert(6);
+	iTree.insert(7);
+	iTree.insert(3);
+	iTree.insert(9);
+
+	for (int i : iTree)
+	{
+		cout << i << endl;
+	}
+
+	system("pause");
+}
